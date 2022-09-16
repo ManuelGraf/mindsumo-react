402399748a2df01b41e4329fd51fbe1b8e286bb9
Manuel Graf <mg@apfelkuh.de>
2022-09-17 00:48:15 +0200

    Deployment

--- src/StorageService.ts
+++ src/StorageService.ts
@@ -1,17 +1,23 @@
-
-export interface Score{
-    score:number,
-    date:number
+export interface Score {
+  score: number;
+  date: number;
+}
+abstract class StorageService {
+  public static saveScore(score) {
+    const scores = JSON.parse(localStorage.getItem("MindSumoScores") || "[]");
+    scores.push({ score: parseInt(score), date: Date.now() });
+    localStorage.setItem("MindSumoScores", JSON.stringify(scores));
   }
-abstract class StorageService{
-  public static saveScore(score){
-    const scores = JSON.parse(localStorage.getItem('MindSumoScores') || '[]');
-    scores.push({score:score,date:Date.now()})
-    localStorage.setItem('MindSumoScores',JSON.stringify(scores));
+  public static getHighScores(limit: number): Score[] {
+    let scores: Score[] = JSON.parse(
+      localStorage.getItem("MindSumoScores") || "[]"
+    ).sort((a, b) => b.score - a.score);
+    return scores.slice(0, limit);
   }
-  public static getHighScores(limit:number):Score[]{
-    let scores:Score[] = JSON.parse(localStorage.getItem('MindSumoScores') || '[]').sort((a,b)=>b.score-a.score);
-    return scores.slice(0,limit);
+  public static fill(count) {
+    for (let index = 0; index < count; index++) {
+      this.saveScore(index);
+    }
   }
 }
-export default StorageService
+export default StorageService;
