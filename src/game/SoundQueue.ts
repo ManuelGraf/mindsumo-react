6a6045efac93098f274af18da09b1b4a95bea82f
Manuel Graf <m.graf@baro-pfannenstein.de>
2022-09-27 01:10:41 +0200

    sound limit

--- src/game/SoundQueue.ts
+++ src/game/SoundQueue.ts
@@ -2,6 +2,8 @@ import { Helper } from "./Helper";
 
 export class SoundQueue{
   keys:string[] = [];
+  treshold=1000;
+  last=0;
   audio;
   constructor(audio:Phaser.Sound.HTML5AudioSoundManager | Phaser.Sound.NoAudioSoundManager | Phaser.Sound.WebAudioSoundManager){
     this.audio = audio;
@@ -10,6 +12,9 @@ export class SoundQueue{
     this.keys=this.keys.concat(keys);
   }
   play(){
-    this.audio.play(this.keys[Helper.randomIntFromInterval(0,this.keys.length-1)],{ volume: 0.2 })
+    if(Date.now()-this.last > this.treshold){
+      this.audio.play(this.keys[Helper.randomIntFromInterval(0,this.keys.length-1)],{ volume: 0.2 })
+      this.last = Date.now();
+    }
   }
 }
